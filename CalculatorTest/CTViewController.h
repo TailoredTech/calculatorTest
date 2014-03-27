a2e8cca89c5a9777af8c132d0fa687ec1fb9c5cb
Aakash Kejriwal <aakash@tailoredtech.in>
2014-03-28 01:50:31 +0530

    Question Set Up

--- CalculatorTest/CTViewController.h
+++ CalculatorTest/CTViewController.h
@@ -7,7 +7,29 @@
 //
 
 #import <UIKit/UIKit.h>
+typedef enum
+{
+    CTButtonTypeInvalid = 0,
+    CTButtonTypeOperand = 1 << 0,
+    CTButtonTypeOpenBracket = 1 << 1,
+    CTButtonTypeCloseBracket = 1 << 2,
+    CTButtonTypeNumber = 1 << 3
+}
+CTButtonType;
 
 @interface CTViewController : UIViewController
 
+@property (nonatomic, weak) IBOutlet UILabel *questionLbl;
+@property (nonatomic, weak) IBOutlet UILabel *answerLbl;
+
+-(IBAction) tappedOnButton:(UIButton *)sender;
+-(IBAction) tappedOnCalculate:(id)sender;
+-(IBAction) clear:(id)sender;
+-(CTButtonType)getTypeForString:(NSString *) str;
+@end
+
+@interface NSMutableArray (Stack)
+-(void) push:(NSString *) obj;
+-(NSString *) pop;
+-(NSString *) peek;
 @end
--- CalculatorTest/CTViewController.m
+++ CalculatorTest/CTViewController.m
@@ -25,5 +25,183 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+-(NSString *) solveQuestion:(NSString *) question
+{
+    return @"Invalid String";
+}
+#pragma mark Utility Function
+//Returns whether operand A has a higher precedence than B
+//For example if a is "x" and b is "+" it return YES
+-(BOOL) operandA:(NSString *) a precedesOperandB:(NSString *)b
+{
+    return ([self scoreForOperand:a]>[self scoreForOperand:b]);
+}
+-(NSInteger) scoreForOperand:(NSString *) str
+{
+    if([str isEqualToString:@"/"] || [str isEqualToString:@"x"])
+    {
+        return 5;
+    }
+    else if([str isEqualToString:@"-"]||[str isEqualToString:@"+"])
+    {
+        return 4;
+    }
+    else
+    {
+        return 3;
+    }
+}
+-(NSString *) stringByRemovingFirstCharacter:(NSString *) str
+{
+    if([str length]>0)
+    {
+        return [str substringFromIndex:1];
+    }
+    return @"";
+
+}
+//check whether  character is operand, opening bracket, closed bracket  or number
+-(CTButtonType)getTypeForString:(NSString *) str
+{
+    if([self checkRegex:@"^(?:\\+|-|\\/|x)$" forString:str])
+    {
+        return CTButtonTypeOperand;
+    }
+    else if([str isEqualToString:@"("])
+    {
+        return CTButtonTypeOpenBracket;
+    }
+    else if([str isEqualToString:@")"])
+    {
+        return CTButtonTypeCloseBracket;
+    }
+    else if([self isNumber:str])
+    {
+        return CTButtonTypeNumber;
+    }
+    return CTButtonTypeInvalid;
+}
+//get last character in a string or return empty string
+-(NSString *) getLastCharacter:(NSString *) str
+{
+    if([str length]>0)
+    {
+        return [str substringFromIndex:[str length]-1];
+    }
+    return @"";
+}
+//get first character
+-(NSString *) getFirstCharacter:(NSString *) str
+{
+    if([str length]>0)
+    {
+        return [str substringToIndex:1];
+    }
+    return @"";
+}
+
+//Perform operation
+-(NSString *) calculateForParamA:(NSString *) aStr paramB:(NSString *) bStr usingOperand:(NSString *) operand
+{
+    CGFloat a = [self floatValue:aStr];
+    CGFloat b = [self floatValue:bStr];
+    CGFloat answer = 0;
+    if([operand isEqualToString:@"/"])
+    {
+        answer = a/b;
+    }
+    else if([operand isEqualToString:@"+"])
+    {
+        answer = a+b;
+    }
+    else if([operand isEqualToString:@"-"])
+    {
+        answer = a-b;
+    }
+    else if([operand isEqualToString:@"x"])
+    {
+        answer = a*b;
+    }
+    return [NSString stringWithFormat:@"%f", answer];
+}
+//String to float
+-(CGFloat) floatValue:(NSString *) str
+{
+    NSNumberFormatter * f = [[NSNumberFormatter alloc] init];
+    [f setNumberStyle:NSNumberFormatterDecimalStyle];
+    NSNumber * myNumber = [f numberFromString:str];
+    return [myNumber floatValue];
+}
+
+-(BOOL) isNumber:(NSString *) str
+{
+    NSNumberFormatter * f = [[NSNumberFormatter alloc] init];
+    [f setNumberStyle:NSNumberFormatterDecimalStyle];
+    NSNumber * myNumber = [f numberFromString:str];
+    return (myNumber!=nil);
+}
+-(BOOL) checkRegex:(NSString *) regex forString:(NSString *) str
+{
+    NSPredicate *regText = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+    return [regText evaluateWithObject:str];
+}
+
+#pragma mark IBActions
+/*
+    Grab Input
+ */
+-(IBAction) tappedOnButton:(UIButton *)sender
+{
+    NSString *text = self.questionLbl.text;
+    NSString *lastCharacter = [self getLastCharacter:text];
+    NSString *newCharacter = sender.titleLabel.text;
+    CTButtonType newCharacterType = [self getTypeForString:newCharacter];
+    CTButtonType lastCharacterType = [self getTypeForString:lastCharacter];
+    if(newCharacterType!=CTButtonTypeNumber || lastCharacterType!=CTButtonTypeNumber)
+    {
+        text = [text stringByAppendingString:sender.titleLabel.text];
+        [self.questionLbl setText:text];
+    }
+}
+
+-(IBAction) tappedOnCalculate:(id)sender
+{
+    NSString *question = self.questionLbl.text;
+    NSString * answer = [self solveQuestion:question];
+    [self.answerLbl setText:answer];
+}
+
+-(IBAction) clear:(id)sender
+{
+    [self.answerLbl setText:@""];
+    [self.questionLbl setText:@""];
+}
+@end
+
+@implementation NSMutableArray (Stack)
+-(void) push:(NSString *) obj
+{
+    [self addObject:obj];
+}
+-(NSString *) pop
+{
+    if([self count]>0)
+    {
+        NSString *topObject = [self lastObject];
+        [self removeLastObject];
+        return topObject;
+    }
+    return nil;
+}
+
+-(NSString *) peek
+{
+    if([self count]>0)
+    {
+        NSString *topObject = [self lastObject];
+        return topObject;
+    }
+    return nil;
+}
 
 @end
