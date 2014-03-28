316438488718c8b4479b0bb276630e795cb10b3f
Aakash Kejriwal <aakash@tailoredtech.in>
2014-03-28 13:55:34 +0530

    Renaming

--- CalculatorTest/CTCalculate.m
+++ CalculatorTest/CTCalculate.m
@@ -18,16 +18,16 @@
     
     while([question length]>0)
     {
-        NSString *nextChar = [question firstElement];// Remove first character from string
+        NSString *nextElement = [question firstElement];// Remove first character from string
         question = [question stringByRemovingFirstElement];
-        CTElementType elementType = [nextChar getElementType];
+        CTElementType elementType = [nextElement getElementType];
         if(elementType==CTElementTypeNumber)//If number add it to numbers stack
         {
-            [numbersStack push:nextChar];
+            [numbersStack push:nextElement];
         }
         else if(elementType==CTElementTypeOpenBracket)//If open bracket add it to the operand stack
         {
-            [operandStack push:nextChar];
+            [operandStack push:nextElement];
         }
         else if(elementType==CTElementTypeCloseBracket)//If close bracket solve equation till last opened bracket
         {
@@ -45,14 +45,14 @@
         else//If operand
         {
             NSString *topOperand = [operandStack peek];
-            if(topOperand != nil && [topOperand precedes:nextChar])//if the previous operand has a higher precedence solve a single calculation
+            if(topOperand != nil && [topOperand precedes:nextElement])//if the previous operand has a higher precedence solve a single calculation
             {
                 if(![self solveSingleOperationNumberStack:numbersStack operandStack:operandStack])
                 {
                     return @"Invalid String";
                 }
             }
-            [operandStack push:nextChar];//add the new operand into the stack
+            [operandStack push:nextElement];//add the new operand into the stack
         }
     }
     if(![self solveTillNextBracketOrNilForNumberStack:numbersStack operandStack:operandStack] && [operandStack peek]==nil && [numbersStack peek] !=nil)//Solve till only one element left in the number stack
